d3110a9affd9e3d93519a8fbfcb2eb9b680a57de
Makc240305 <maxim240305@gmail.com>
2025-02-20 12:31:27 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="overflow-x-hidden text-stone-200 antialiased">
+      <div className="fixed inset-0 -z-10"></div>
+    </div>
+  );
+};
+
+export default App;
