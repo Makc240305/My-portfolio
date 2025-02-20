6a43a20535dae183c500bec85e0c925b3c7e0430
Makc240305 <maxim240305@gmail.com>
2025-02-20 13:34:46 +0100

    added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,19 @@
-
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
         <a href="/" aria-label="Home">
-          <img src={'logo'} className="mx-2" width={50} height={50} alt="Logo" />
+          <img src='logo' className="mx-2" width={50} height={50} alt="Logo" />
         </a>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <a href="/" aria-label="Home">
-          <img src={'logo'} className="mx-2" width={50} height={50} alt="Logo" />
+        <a href="https://github.com/Makc240305" aria-label="GitHub" target='_blank'> 
+          <FaGithub />
+        </a>
+        <a href="https://github.com/Makc240305" aria-label="Linkedin" target='_blank'> 
+          <FaLinkedin />
         </a>
       </div>
     </nav>
