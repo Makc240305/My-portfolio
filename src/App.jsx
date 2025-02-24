888d36921fc0a0a5cd139ffed3bec6975023fe01
Makc240305 <maxim240305@gmail.com>
2025-02-25 00:47:13 +0100

    added new bg

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,12 @@ const App = () => {
   return (
     <div className="overflow-x-hidden text-stone-200 antialiased">
       <div className="fixed inset-0 -z-10">
-        <div className="relative h-full w-full bg-neutral-900"></div>
-        <div className="absolute inset-0 bg-grey-500  bg-[size:20px_20px] opacity-20 blur-[100px]"></div>
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
+        <div className="relative h-full w-full bg-black">
+          <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+          <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+        </div>
       </div>
+
       <div className="container mx-auto px-8">
         <Navbar />
         <Hero />
