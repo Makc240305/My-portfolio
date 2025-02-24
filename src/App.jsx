b4b189b52a2e177ab24552fb13313633969b60a7
Makc240305 <maxim240305@gmail.com>
2025-02-25 00:24:03 +0100

    added Experience section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Technologies from "./components/Technologies";
 import Projects from "./components/Projects";
+import Experience from "./components/Experience";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Hero />
         <Technologies />
         <Projects />
+        <Experience />
       </div>
     </div>
   );
