5873a1299b0680dae0a79067af68d5cbd2e51d47
Makc240305 <maxim240305@gmail.com>
2025-02-27 21:28:47 +0100

    Remaked image sizes on the computer and mobile

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,41 +11,47 @@ const Projects = () => {
             key={index}
             className="mb-12 flex flex-col jusify-start gap-8 lg:flex-row flex-nowrap"
           >
-            <div className="self-center lg:w-1/4">
-              <a 
-                href={project.link} 
-                aria-label={`Visit ${project.title} project`} 
-                target="_blank" 
+            <div className="self-center lg:w-3/8">
+              <a
+                href={project.link}
+                aria-label={`Visit ${project.title} project`}
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <img
                   src={project.image}
-                  width={250}
-                  height={250}
                   alt={project.title}
                   className="rounded"
                 />
               </a>
             </div>
-            <div className="flex flex-wrap justify-between gap-6 lg:flex-col w-full">
-              <div>
-                <a 
-                  href={project.link} 
-                  aria-label={`Open ${project.title} project`} 
-                  target="_blank" 
+            <div className="flex flex-wrap justify-between gap-6 w-full relative lg:flex-col">
+            <div className="text-3xl absolute right-0 top-0">
+                <a
+                  href={project.githubLink}
+                  aria-label={`View ${project.title} on GitHub`}
+                  title="View on GitHub"
+                  target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <h3 
+                  <FaGithub />
+                </a>
+              </div>
+              <div>
+                <a
+                  href={project.link}
+                  aria-label={`Open ${project.title} project`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <h3
                     className="mb-2 font-semibold text-2xl"
                     id={`project-title-${index}`}
                   >
                     {project.title}
                   </h3>
                 </a>
-                <p 
-                  className="mb-4 text-stone-400" 
-                  id={`project-desc-${index}`}
-                >
+                <p className="mb-4 text-stone-400" id={`project-desc-${index}`}>
                   {project.description}
                 </p>
                 {project.technologies.map((technology, i) => (
@@ -56,17 +62,6 @@ const Projects = () => {
                     {technology}
                   </span>
                 ))}
-              </div>
-              <div className="text-3xl md:self-end lg:self-end">
-                <a
-                  href={project.githubLink}
-                  aria-label={`View ${project.title} on GitHub`}
-                  title="View on GitHub"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaGithub />
-                </a>
               </div>
             </div>
           </div>
